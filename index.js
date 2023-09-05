821810d3160336708377167c920539eed83794c1
joosun0823 <133857203+joosun0823@users.noreply.github.com>
2023-09-05 12:48:41 +0900

    sdf

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
-var express = require('express')
-var cors = require('cors')
-const fs = require("fs")
-var bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const { log } = require('console');
+const app = express();
 
-var app = express()
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+const data = {
+  select : function() {
+    return JSON.parse(fs.readFileSync('./test.json'));
+  },
+  insert : function(newObj) {
+      const jsonData = data.select();
+      let newData = [...jsonData, {id:jsonData.length+1, ...newObj}]
+      fs.writeFileSync('./test.json',JSON.stringify(newData)  );
+      return newData;
+  },
+  update : function() {},
+  delete : function() {}
+}
+
 app.get('/abc', function (req, res) {
-    const jsonData = fs.readFileSync("./test.json");
-    res.send( JSON.parse(jsonData) );
+  
+  res.send( data.select() )
+})
+
+app.delete('/abc/:id', function (req, res) {
+  const jsonData = data.select();
+  const {id} = req.params;
+  const delData = jsonData.filter(n=>n.id != id)
+  
+  fs.writeFileSync('./test.json', JSON.stringify( delData )  );
+  res.send(  delData  )
 })
 
 app.post('/insert', function (req, res) {
-    const jsonData = fs.writeFileSync("./test.json", JSON.stringify(req.body));
-    res.send( "성공" );
-})
+  
+  res.send( data.insert(req.body) )
+});
 
-
-app.listen(3030)
+app.listen(3000)
